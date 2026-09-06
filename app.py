859360d75708990d277e67bf8d04b203fad46290
Alirezandri <alirezanaderip21@gmail.com>
2026-09-06 22:17:45 +0330

    Add publications page and update navigation

--- app.py
+++ app.py
@@ -12,6 +12,9 @@ def home():
         name=name
     )
 
+@app.route("/publications")
+def publications():
+    return render_template("publications.html")
 
 @app.route("/robots.txt")
 def robots():
